9dbebb46b1d48bf0d74d69835b3d69c28d0207f3
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-21 19:26:07 +0200

    fix: correct import path for Home component

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import About from './pages/About'
 
 createRoot(document.getElementById('root')!).render(
