0d311b477ca84e144ed10dd713cad025581c2897
Oignontom8283 <tfarin83@gmail.com>
2025-07-24 02:40:45 +0200

    refactor: restructure components; move Navbar and Footer to components directory

--- src/Footer.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-  return (
-    <div>
-      <p>Footer content goes here</p>
-    </div>
-  )
-}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,22 @@
-
 export default function Home() {
-  return <div>
-    <h1 className="text-red-50">Home Page</h1>
-  </div>
+  return (
+    <div className="relative min-h-screen">
+      {/* Vidéo background */}
+      <video
+        src="assets/background.mp4"
+        autoPlay
+        loop
+        muted
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10"
+      />
+
+      {/* Content */}
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-white">
+        <div>
+          <img src="logo.png" alt="Logo" />
+          <h1 className="text-4xl font-bold mb-4">Loading...</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
