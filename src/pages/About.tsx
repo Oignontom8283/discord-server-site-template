0b04af3b8fa19fbe9684178da267e0922aab8311
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-27 19:07:40 +0200

    feat: update document title handling and enhance Articles and Home components with dynamic data rendering

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,15 @@
+import { useContext, useEffect } from "react"
+import { DataContext } from "../context";
 
 export default function About() {
-    return <div>
-        <h1>About Page</h1>
-        <p>This is the about page of the Discord server site template.</p>
-    </div>
+
+  const { data } = useContext(DataContext);
+
+  useEffect(() => {
+    document.title = "About - " + data?.invite.guild.name;
+  }, [])
+
+  return <div>
+
+  </div>
 }
--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,4 +1,18 @@
+import { useContext } from "react";
+import { DataContext } from "../context";
+import Alert from "../components/Alert";
+
 export default function Articles() {
+  const { data } = useContext(DataContext);
+
+  if (!data?.config.article) {
+    return (
+      <div className="flex-1 flex items-center justify-center bg-red-100">
+        <Alert alertType="warning">Sorry, the articles have been disabled by the site administrator(s).</Alert>
+      </div>
+    )
+  }
+  
   return (
     <div className="bg-red-500">
       <h1>Articles</h1>
