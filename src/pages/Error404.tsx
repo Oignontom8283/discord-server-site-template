51e27af47da1c38519f9e218392936f5ff789c0e
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-29 03:25:14 +0200

    feat: update Error404 page layout and set document title for better user experience

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,12 +1,18 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Error404() {
+
+  useEffect(() => {
+    document.title = "404 - Page Not Found";
+  }, []);
+
   return (
     <div className="flex-1 flex items-center justify-center bg-red-100">
       <div className="bg-base-100 rounded-box shadow-lg p-5">
         <h1 className="text-2xl font-bold mb-3">404 - Page Not Found</h1>
         <p className="text-gray-700">Sorry, the page you are looking for does not exist.</p>
-        <Link to="/" className="btn btn-primary">Go back to Home</Link>
+        <Link to="/" className="btn btn-accent mt-6">Go back to Home</Link>
       </div>
     </div>
   );
